052d4233b6b3cfb02c3a134c5a1d674dcafde48c
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-01-23 13:53:14 -0600

    Add build_tree and pretty_print methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,4 +6,21 @@ class Tree
   def initialize(array)
     @root = build_tree(array)
   end
+
+  def build_tree(array)
+    return nil if array.empty?
+    array = array.uniq.sort
+    middle = array.size / 2
+    root_node = Node.new(array[middle])
+    root_node.left = build_tree(array[0...middle])
+    root_node.right = build_tree(array[middle + 1..-1])
+    root_node
+  end
+
+  # From The Odin Project
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
