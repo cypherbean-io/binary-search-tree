973c92c8d96cb57a686cc55d57fac06948dd6199
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-01-24 18:42:04 -0600

    Add delete and find_minimum methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -33,4 +33,25 @@ class Tree
     end
     node
   end
+
+  def delete(value, node = @root)
+    return node if node.nil?
+    if value < node.data
+      node.left = delete(value, node.left)
+    elsif value > node.data
+      node.right = delete(value, node.right)
+    else
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+      minimum_node = find_minimum(node.right)
+      node.data = minimum_node.data
+      node.right = delete(minimum_node.data, node.right)
+    end
+    node
+  end
+
+  def find_minimum(node)
+    node = node.left until node.left.nil?
+    node
+  end
 end
