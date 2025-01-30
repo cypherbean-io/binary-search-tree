38d441c8987176da2e1a6c65b50dbe2cf679f190
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2025-01-29 20:25:18 -0600

    Add balanced? method

--- lib/tree.rb
+++ lib/tree.rb
@@ -110,4 +110,11 @@ class Tree
       depth(node, target.right, edges + 1)
     end
   end
+
+  def balanced?(node = @root)
+    return true if node.nil?
+    left_height = height(node.left)
+    right_height = height(node.right)
+    (left_height - right_height).abs <= 1 && balanced?(node.left) && balanced?(node.right)
+  end
 end
